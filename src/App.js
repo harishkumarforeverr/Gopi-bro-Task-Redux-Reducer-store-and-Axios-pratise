f57d93caf720401b67c50f28b86efdadfe72ed3f
harishkumarforever <h.nunavath@dev2prod.com>
2022-01-05 14:27:01 -0500

    added table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.scss";
 import "antd/dist/antd.css";
-import { Button, Input } from "antd";
+import { Button, Input, Table } from "antd";
 
 const App = () => {
   const [formDate, setFormDate] = useState([]);
@@ -78,41 +78,96 @@ const App = () => {
       setShowAdd(true);
     }
   };
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Edit",
+      dataIndex: "Edit",
+      key: "Edit",
+      render: (text, record) => (
+        <Button type="primary"
+          onClick={() => {
+            // console.log(record);
+            handleEditUser(record.id);
+          }}
+        >
+   Edit
+        </Button>
+      ),
+    },
+    {
+      title: "Delete",
+      dataIndex: "Delete",
+      key: "Delete",
+      render: (text, record) => (
+        <Button type="primary" danger
+          onClick={() => {
+            // console.log(record);
+            handleRemoveUser(record.id);
+          }}
+        >
+        Delete
+        </Button>
+      ),
+    },
+  ];
   return (
     <div>
       <div className="container">
         <h1> Welcome To Survery Record </h1>
-        <label> Username : </label>
-        <Input
-          name="name"
-          value={inputfields.name}
-          onChange={handleInputChange}
-          type="text"
-        />
-
-        <label> Age : </label>
-        <Input
-          value={inputfields.age}
-          onChange={handleInputChange}
-          name="age"
-          type="number"
-        />
-        {ShowAdd ? (
-          <button type="primary" onClick={handleAddSubmit} type="submit">
-            {" "}
-            Add{" "}
-          </button>
-        ) : (
-          <button onClick={handleUpdateSubmit} type="submit">
-            {" "}
-            Update{" "}
-          </button>
-        )}
+        <div className="container__items">
+          <div>
+            <label> Username : </label>
+            <Input
+              name="name"
+              value={inputfields.name}
+              onChange={handleInputChange}
+              type="text"
+            />
+          </div>
+          <div>
+            <label> Age : </label>
+            <Input
+              value={inputfields.age}
+              onChange={handleInputChange}
+              name="age"
+              type="number"
+            />
+          </div>
+          <div>
+            {ShowAdd ? (
+              <Button 
+              className="add"
+              type="primary" type="primary" onClick={handleAddSubmit} type="submit">
+                {" "}
+                Add{" "}
+              </Button>
+            ) : (
+              <Button 
+              
+              className="update"
+              onClick={handleUpdateSubmit} type="submit">
+                {" "}
+                Update{" "}
+              </Button>
+            )}
+          </div>
+        </div>
       </div>
       <div>
         {formDate ? (
           <div>
-            {formDate.map((item) => {
+            {/* {formDate.map((item) => {
               return (
                 <div className="users" key={item.id}>
                   <h1> {item.name}</h1>
@@ -137,7 +192,13 @@ const App = () => {
                   </Button>
                 </div>
               );
-            })}
+            })} */}
+            <Table
+              key="table"
+              columns={columns}
+              dataSource={formDate}
+              pagination={false}
+            />
           </div>
         ) : (
           <p> No Users Record Found</p>
